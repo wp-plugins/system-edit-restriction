1301e5091d24e40fecde4fa918f0a589ef729850
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-10 08:32:39 +0000

    uouert
    git-svn-id: https://plugins.svn.wordpress.org/system-edit-restriction/trunk@1004956 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== System edit restriction ===
+Contributors: selnomeria
+Tags: disable, system,plugins,theme,edit,modification,restrict,protect
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 0.1
+
+No-one will be able edit/modify system files(theme+plugins) from Dashboard,even admins. Only from FTP can be edited... Secure to your site!!
+
+
+== Description ==
+
+It is useful, when you share Admin access to others - They wont be able to modify system PHP files.
+
+
+== Installation ==
+
+1. Install plugin from "ADD NEW" or Upload directory to the `/wp-content/plugins/` directory.
+
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+None.
+
+== Screenshots ==
+None.
+
+== Changelog ==
+
+= 0.1.0 =
+* First release edition.
---
+++ system-edit-restriction.php
@@ -0,0 +1,223 @@
+<?php
+/*
+Plugin Name: System edit restriction
+Description: No-one will be able edit/modify system files(theme+plugins) from Dashboard,even admins. Only from FTP can be edited... Secure to your site!!  It is useful, when you share Admin access to others - They wont be able to modify system PHP files.
+contributors: selnomeria
+Version: 1.1
+License: GPLv2
+*/
+$GLOBALS['SystemEditProtectionzzz'] = new SystemEditRestriction;
+
+class SystemEditRestriction
+{
+	//const ID = 'protect';
+	protected $fileName = 'login_protection_';
+	protected $StartSYMBOL		='<?php //';
+
+	
+	
+	
+	public function __construct()
+	{
+		add_action('admin_menu', array($this,'add_menu_buttttton'));
+		add_action('admin_init', array($this,'start_admin_restrict_checkerr'));
+		register_activation_hook( __FILE__,  array($this, 'sep_activate'));
+		register_deactivation_hook( __FILE__,  array($this, 'sep_deactivate'));
+	}
+	
+	
+	
+	public function blockedMessage()
+	{
+		return  "(HOWEVER,IF YOU BLOCK YOURSELF, enter your wordpress directory (from FTP) and add your IP into this file: /ALLOWED_IP/ALLOWED_IPs_FOR_WP_MODIFICATION.php)";
+	}
+	
+	public function domainn()
+	{
+		return str_replace('www.','', $_SERVER['HTTP_HOST']);
+	}	
+	
+	
+	public function check_enable_privilegies()
+	{
+		$allwd_ips = file_get_contents($this->allowed_ips_filee());
+
+		//check if FILE CONTAINS USER'S IP. if IP not found, then check, if PLUGIN should restrict him anyway.
+		if (strpos($allwd_ips, $_SERVER['REMOTE_ADDR']) === false){
+			//check if restriction is ENABLED
+			if (get_option('optin_for_sep_ipss') == 2){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	public function start_admin_restrict_checkerr()
+	{
+		if (!$this->check_enable_privilegies())
+		{
+			define( 'DISALLOW_FILE_MODS', true );
+				//remove_menu_page( 'edit-comments.php' );
+				//remove_menu_page( 'themes.php' );
+				//remove_menu_page( 'plugins.php' );
+				//remove_menu_page( 'admin.php?page=mp_st' );
+				//remove_menu_page( 'admin.php?page=cp_main' );
+				//remove_submenu_page( 'edit.php?post_type=product', 'edit-tags.php?taxonomy=product_category&amp;post_type=product' );
+			
+			
+			$restrictions = array(
+			'/wp-admin/options-general.php?page=my-system-edit-restriction',
+			'/wp-admin/widgets.php',
+			'/wp-admin/widgets.php',
+			'/wp-admin/user-new.php',
+			'/wp-admin/upgrade-functions.php',
+			'/wp-admin/upgrade.php',
+			'/wp-admin/themes.php',
+			'/wp-admin/theme-install.php',
+			'/wp-admin/theme-editor.php',
+			'/wp-admin/setup-config.php',
+			'/wp-admin/plugins.php',
+			'/wp-admin/plugin-install.php',
+			'/wp-admin/options-head.php',
+			'/wp-admin/network.php',
+			'/wp-admin/ms-users.php',
+			'/wp-admin/ms-upgrade-network.php',
+			'/wp-admin/ms-themes.php',
+			'/wp-admin/ms-sites.php',
+			'/wp-admin/ms-options.php',
+			'/wp-admin/ms-edit.php',
+			'/wp-admin/ms-delete-site.php',
+			'/wp-admin/ms-admin.php',
+			'/wp-admin/moderation.php',
+			'/wp-admin/menu-header.php',
+			'/wp-admin/menu.php',
+			'/wp-admin/edit-comments.php'
+			);
+
+			foreach ( $restrictions as $restriction ) {
+				if ( strpos($_SERVER['REQUEST_URI'],$restriction) !== false) {
+					die('no access to this page. error_534 ... <a href="./">Go Back</a> <br/><br/>'.$this->blockedMessage());
+				}
+			}
+		}
+	
+	}
+	
+	
+	public function sep_activate()
+	{
+		//
+	}	
+	public function sep_deactivate()
+	{
+		unlink($this->allowed_ips_filee());
+	}	
+	
+	
+	public function allowed_ips_filee()
+	{
+		//initial values
+		$bakcup_of_ipfile = get_option("backup_allowed_ips_modify_". $this->domainn() );
+		$Value = !empty($bakcup_of_ipfile)?  $bakcup_of_ipfile : $this->StartSYMBOL. '101.101.101.101 (its James, my friend)|||102.102.102.102(its my pc),';
+		
+		//file path
+		$pt_folder = ABSPATH.'/ALLOWED_IP/'. $this->domainn();		if(!file_exists($pt_folder)){mkdir($pt_folder, 0755, true);}
+		$file = $pt_folder .'/ALLOWED_IPs_FOR_WP_MODIFICATION.php';	if(!file_exists($file))		{file_put_contents($file, $Value);}
+		return $file;
+	}
+	
+	public function add_menu_buttttton() {
+		add_submenu_page( 'options-general.php', 'System Edit Restrict', 'System Edit Restriction', 'manage_options', 'my-system-edit-restriction', array($this,'sep_output') ); 
+	}
+
+	
+	
+
+	public function sep_output()
+	{
+	?>
+	
+	
+	
+			<!-- ENABLE/DISABLE OPTIONS -->
+			<?php
+			//IF whitelist updated
+			if (!empty($_POST['opt_of_whitelist_ips'])) 
+			{
+				//update setting
+				update_option('optin_for_sep_ipss',$_POST['opt_of_whitelist_ips']);
+				//change IP file
+					$final	= $_POST['sep_white_IPS'];
+					$final	= str_replace("\r\n\r\n",	"",		$final);
+					$final	= str_replace("\r\n",		"|||",	$final);
+				file_put_contents($this->allowed_ips_filee(), $this->StartSYMBOL .$final );
+				//make backup
+				update_option("backup_allowed_ips_modify_". $this->domainn() ,  $this->StartSYMBOL .$final);
+			}
+		
+			$allowed_ips 	= str_replace($this->StartSYMBOL, '', file_get_contents($this->allowed_ips_filee()) );
+			$whiteip_answer	= get_option('optin_for_sep_ipss');
+			$d2 = $whiteip_answer == 2 ? "checked" : '';
+			$d1 = $whiteip_answer == 1 || empty($whiteip_answer) ? "checked" : '';
+			?>
+			<br/><br/>	
+			<form method="post" action="">
+				<p class="submit">
+					
+					<div class="white_list_ipps" style="background-color: #1EE41E;padding: 5px; margin:0 0 0 20%;width: 50%;">
+						<div style="font-size:1.2em;font-weight:bold;">
+							RESTRICT PLUGIN/THEME EDIT&INSTALL from DASHBOARD: (<a href="javascript:alert('1)OFF - No changes. any admin will have a full access. \r\n2) ON - Only the listed IPs can  EDIT&INSTALL PLUGINS or THEMES. Another IP (even if he is admin) cant EDIT&INSTALL them. <?php echo $this->blockedMessage();?> \r\n');">read more!!</a>):
+						</div>
+						<table style="border: 1px solid;"><tbody>
+							<tr><td>OFF </td><td><input onclick="lg_radiod();" type="radio" name="opt_of_whitelist_ips" value="1" <?php echo $d1;?> /></td></tr>
+							<tr><td>ON</td><td><input onclick="lg_radiod();" type="radio" name="opt_of_whitelist_ips" value="2" <?php echo $d2;?> /></td></tr>
+						</tbody></table>
+						<div style="float:right;">(your IP is <b style="color:red; background-color:yellow;"><?php echo $_SERVER['REMOTE_ADDR'];?></b>)</div>
+						<br/>
+						
+						<div id="DIV_whiteipielddd" style="overflow-y:auto;">
+							<?php
+							$liness=explode("|||",$allowed_ips);
+							?>
+							<textarea id="whiteips_fieldd" style="width:100%;height:150px;" name="sep_white_IPS"><?php foreach ($liness as $line) {echo $line."\r\n";}?></textarea>
+						</div>
+						
+						<script type="text/javascript">
+						function lg_radiod()
+						{
+							var valllue = document.querySelector('input[name="opt_of_whitelist_ips"]:checked').value;
+							var DIVipfieldd = document.getElementById("DIV_whiteipielddd");
+
+							if(valllue != "1")	{DIVipfieldd.style.opacity = "1";}
+							else				{DIVipfieldd.style.opacity = "0.3";	}
+						}
+						lg_radiod();
+						</script>
+					</div>
+
+					<br/><div style="clear:both;"></div>
+					<input type="submit"  value="SAVE" onclick="return check_sep_ips();" />
+					<script type="text/javascript">
+					function check_sep_ips()
+					{
+						var IPLIST_VALUE=document.getElementById("whiteips_fieldd").value;
+						var user_ip="<?php echo $_SERVER['REMOTE_ADDR'];?>";
+						
+						var TurnedONOFF = document.querySelector('input[name="opt_of_whitelist_ips"]:checked').value;
+						if (TurnedONOFF != "1")
+						{
+							if (IPLIST_VALUE.indexOf(user_ip) == -1)
+							{
+								if(!confirm("YOUR IP(" + user_ip +") is not in list! Are you sure you want to continue?")){return false;}
+							}
+						}
+						return true;
+					}
+					</script>
+				</p> 
+	
+	
+	<?php
+	}
+}
